36f9336f2e4ed01a61b7df93f18ba583add9b608
sharppi <juhana.ovaska@gmail.com>
2017-01-19 00:19:20 +0200

    Update home.cljs

--- src/cljs/dungeon_simulator/pages/home.cljs
+++ src/cljs/dungeon_simulator/pages/home.cljs
@@ -6,8 +6,9 @@
    [title/render "Dungeon Simulator"]
 
    [:div
-    [:p "This website is a dungeon simulator for world's oldest role playing game. It enables you to play a dungeon
-         crawl type of game with your friends without the need for a dungeon master or as a solo adventure by yourself."]
+    [:p "Welcome to Dungeon Simulator! This game is for you, if you like to play dungeon crawlers
+         or for those who would like to test out your own scenarios without the need for a dungeon master or as a solo 
+         adventure by yourself."]
 
     [:p "You use your character sheet and roll dice as with regular game. This simulator will not keep up with the state
          of your character sheet, inventory or results of your skill checks. It merely acts as a replacement for a
@@ -16,7 +17,7 @@
     [:p "Once you start a game, it is a running dungeon adventure and as such, it has no 'Save' option. If you return
          to Dungeon Simulator at some point, a new adventure will commence from the scratch."]
 
-    [:p "You will not need monster catalog, all NPC statistics will be provided upon confrontation with them."]
+    [:p "You won't need a monster catalog, all NPC statistics will be provided upon the confrontation with them."]
 
     [:p "Enjoy the game and may the dice be in your favor!"]]
 
