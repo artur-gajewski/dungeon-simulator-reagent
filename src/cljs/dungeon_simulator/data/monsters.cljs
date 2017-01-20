06482be3c3d4b3137fd042fe05c871c7cc698528
Artur Gajewski (fraktio) <artur.gajewski@fraktio.fi>
2017-01-20 08:39:08 +0200

    Added NPC stat blocks to start off with

--- src/cljs/dungeon_simulator/data/monsters.cljs
+++ src/cljs/dungeon_simulator/data/monsters.cljs
@@ -7,77 +7,138 @@
                {:roll-from 50
                 :roll-to 53
                 :type "Rat"
-                :distance 5
-                :hp 2}
+                :xp 80
+                :distance -1
+                :ac 0
+                :hp {:static 8
+                     :roll-amount 2
+                     :roll-sides 8}
+                :stats {:speed 0
+                        :strength 0
+                        :dexterity 0
+                        :constitution 0
+                        :intelligence 0
+                        :wisdom 0
+                        :charisma 0}
+                :senses [{:type "Passive Perception"
+                          :value "10"
+                          :unit ""}
+                         {:type "Darkvision"
+                          :value "60"
+                          :unit "feet"}]
+                :languages [{:type "common"
+                             :description "Common"}
+                            {:type "frogfolk"
+                             :description "Frogfolk, understands some Finnish"}]
+                :weapons {:melee [{:description "Greatclub"
+                                   :to-hit +6
+                                   :reach {:value 5
+                                           :unit "feet"}
+                                   :hit {:static 8
+                                         :roll-amount 1
+                                         :roll-sides 8
+                                         :roll-modifier +4}}]
+                          :range [{:description "Spear"
+                                   :to-hit +4
+                                   :reach {:value 30
+                                           :unit "feet"}
+                                   :hit {:static 10
+                                         :roll-amount 1
+                                         :roll-sides 6
+                                         :roll-modifier +4}}]}}
 
                {:roll-from 54
                 :roll-to 57
                 :type "Giant Spider"
-                :distance 5
+                :xp: 0
+                :distance -1
+                :ac 0
                 :hp 5}
 
                {:roll-from 58
                 :roll-to 61
                 :type "Bat"
-                :distance 5
+                :xp: 0
+                :distance -1
+                :ac 0
                 :hp 3}
 
                {:roll-from 62
                 :roll-to 65
                 :type "Snake"
-                :distance 5
+                :xp: 0
+                :distance -1
+                :ac 0
                 :hp 4}
 
                {:roll-from 66
                 :roll-to 69
                 :type "Ghost"
-                :distance 5
+                :xp: 0
+                :distance -1
+                :ac 0
                 :hp 7}
 
                {:roll-from 70
                 :roll-to 73
                 :type "Minor Demon"
-                :distance 5
+                :xp: 0
+                :distance -1
+                :ac 0
                 :hp 10}
 
                {:roll-from 74
                 :roll-to 77
                 :type "Goblin"
-                :distance 5
+                :xp: 0
+                :distance -1
+                :ac 0
                 :hp 6}
 
                {:roll-from 78
                 :roll-to 81
                 :type "Hobgoblin"
-                :distance 5
+                :xp: 0
+                :distance -1
+                :ac 0
                 :hp 7}
 
                {:roll-from 82
                 :roll-to 85
                 :type "Kobold"
-                :distance 5
+                :xp: 0
+                :distance -1
+                :ac 0
                 :hp 4}
 
                {:roll-from 86
                 :roll-to 89
                 :type "Skeleton"
-                :distance 5
+                :xp: 0
+                :distance -1
+                :ac 0
                 :hp 5}
 
                {:roll-from 90
                 :roll-to 93
                 :type "Orc"
-                :distance 5
+                :xp: 0
+                :distance -1
+                :ac 0
                 :hp 7}
                
                {:roll-from 94
                 :roll-to 97
                 :type "Ghoul"
-                :distance 5
+                :xp: 0
+                :distance -1
+                :ac 0
                 :hp 10}
 
                {:roll-from 98
                 :roll-to 100
                 :type "Zombie"
-                :distance 5
+                :xp: 0
+                :distance -1
+                :ac 0
                 :hp 8}])
