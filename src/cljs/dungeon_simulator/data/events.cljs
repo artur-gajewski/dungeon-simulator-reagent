30a0dd262da10bef930811de26286a71f315d032
Artur Gajewski (fraktio) <artur.gajewski@fraktio.fi>
2017-01-20 21:44:44 +0200

    Added events and items data files

---
+++ src/cljs/dungeon_simulator/data/events.cljs
@@ -0,0 +1,14 @@
+(ns dungeon-simulator.data.events)
+
+(def events [
+
+             {:roll-from 1
+              :roll-to 49
+              :type "None"}
+
+             {:roll-from 50
+              :roll-to 100
+              :type "Foo"
+              :description "Lorem ipsum sippala sippala bada boom"}
+
+             ])
