d178132ff930861d92904d1c5b7ba2e43610df99
Artur Gajewski (fraktio) <artur.gajewski@fraktio.fi>
2017-01-21 17:40:03 +0200

    Modified phrasing

--- src/cljs/dungeon_simulator/components/legend.cljs
+++ src/cljs/dungeon_simulator/components/legend.cljs
@@ -6,5 +6,5 @@
    (when-not (= (-> monster :type) "None")
      [:p
       (str
-        "There's " (-> monster :type) " " (-> monster :distance) " feet from you. If you commence combat, "
+        "There is " (-> monster :type) " " (-> monster :distance) " feet from you. If you commence combat, "
         (-> monster :type) " will attack with the best weapon available concidering it's distance from you.")])])
