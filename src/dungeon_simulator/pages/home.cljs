bbeba968b3e421931d8d5d0b475e702627ddd730
Artur Gajewski (fraktio) <artur.gajewski@fraktio.fi>
2017-01-25 10:27:21 +0200

    Updated intro text

--- src/dungeon_simulator/pages/home.cljs
+++ src/dungeon_simulator/pages/home.cljs
@@ -5,15 +5,18 @@
   [:div
    [:h2 "Dungeon Simulator"]
    [:div
-    [:p "Welcome to Dungeon Simulator! This game is for you, if you like to play dungeon crawlers
-         or for those who would like to test out your own scenarios without the need for a dungeon master or as a solo 
-         adventure by yourself."]
-    [:p "You use your character sheet and roll dice as with regular game. This simulator will not keep up with the state
-         of your character sheet, inventory or results of your skill checks. It merely acts as a replacement for a
-         living dungeon master to give you structure for the adventure."]
-    [:p "Once you start a game, it is a running dungeon adventure and as such, it has no 'Save' option. If you return
-         to Dungeon Simulator at some point, a new adventure will commence from the scratch."]
-    [:p "You won't need a monster catalog, all NPC statistics will be provided upon the confrontation with them."]
-    [:p "Enjoy the game and may the dice be in your favor!"]]
+    [:p "Welcome to Dungeon Simulator! So you want to be a dungeon hero, eh? Let me tell you, it won't be easy!"]
+    [:p "Grab your role playing game's character sheet and roll dice as with any regular RPG. This simulator will not
+         keep up with the state of your character sheet, inventory or results of your skill checks. What it will do,
+         is enable you to test out your newly created RPG character or play dungeon crawl type of solo adventure."]
+    [:p "Rooms, NPCs, situations and events all have dynamic data. For example, you might confront same type of
+         monsters multiple times, but each time they will have different initiative, distance, awareness, hit points
+         and many other things alike. You might want to attack or you might want to try and stay away from fighting if
+         your hit points are low. Or you might want risk it and fight just to obtain that healing potion behind the
+         monster. It all depends on the situation you are in and what you want to do."]
+    [:p "Once you click below link, you start a running dungeon adventure and as such, it has no 'Save' option.
+         If you return to Dungeon Simulator at some point, a new adventure will begin from the scratch."]
+    [:p "You won't need monster catalogs, all NPC statistics will be provided upon the confrontation with them."]
+    [:p "Stay alive, enjoy the dungeon experience and may the dice be in your favor!"]]
    [:div {:class "options"}
-    [:a {:href "/room"} "» Start new adenture"]]])
+    [:a {:href "/room"} "» Start new adventure"]]])
